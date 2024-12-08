db82c8a2af942d8b62112511d14eb87622411036
chairbender <kwhipke1@gmail.com>
2024-12-08 12:25:14 -0800

    sexyparo

---
+++ README.md
@@ -0,0 +1,5 @@
+# mame-lua
+
+Various mame lua scripts for certain games.
+
+* sexyparo-rank - show timer and total rank for Sexy Parodius (JAA)
--- sexyparo-rank.lua
+++ sexyparo-rank.lua
@@ -1,3 +1,5 @@
+-- For Sexy Parodius (JAA), shows the timer rank and total rank on the screen
+
 for tag, screen in pairs(manager.machine.screens) do print(tag) end
 cpu = manager.machine.devices[":maincpu"]
 mem = cpu.spaces["program"]
@@ -9,8 +11,8 @@ total_rank_addr = 0xc109ab
 function draw_overlay()
     timer_rank = mem:read_u8(timer_rank_addr)
     total_rank = mem:read_u8(total_rank_addr)
-    s:draw_text(40, 40, timer_rank)
-    s:draw_text(80, 80, total_rank)
+    s:draw_text(40, 0, "Timer: " .. timer_rank)
+    s:draw_text(200, 0, "Total: " .. total_rank)
 end
 
 emu.register_frame_done(draw_overlay, 'frame')
